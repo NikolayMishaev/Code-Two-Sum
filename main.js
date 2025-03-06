f3055e33c80219a38143a18a5d2a1c86f749700b
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2025-03-06 21:42:54 +0300

    feat: функция реализована

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /* 
 
@@ -13,3 +13,14 @@
 bugfix:
 
 */
+
+function twoSum(numbers, target) {
+  for (let i = 0; i <= numbers.length - 2; i++) {
+    for (let j = i + 1; j <= numbers.length - 1; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}
+
+console.log(twoSum([1, 2, 3], 4)) // [0, 2]
+console.log(twoSum([3, 2, 4], 6)) // [1, 2]
